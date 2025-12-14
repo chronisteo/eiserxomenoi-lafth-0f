b7aad0d8f828d0a3cde3b96444c095edebcba30a
FilipposDe <philipposdem@hotmail.com>
2025-12-14 17:44:45 +0200

    fix: change Loading component to default export

--- app/login/loading.tsx
+++ app/login/loading.tsx
@@ -1,4 +1,4 @@
-export function Loading() {
+export default function Loading() {
 	return (
 		<div className='min-h-screen bg-background flex items-center justify-center'>
 			<div className='loader'>Φόρτωση...</div>
